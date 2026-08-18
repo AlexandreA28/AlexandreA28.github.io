1d7cdad44e7e52796770fe8cb1c80fe0d1d5f12d
AlexandreA28 <sniper28.alex@gmail.com>
2026-08-19 00:26:21 +0200

    Ajustement de la barre de chargement

--- assets/js/load.js
+++ assets/js/load.js
@@ -2,9 +2,8 @@ window.addEventListener('load', () => {
     const loader = document.getElementById('loader');
     
     if (loader) {
-        // On ajoute un tout petit délai (ex: 400ms) pour que l'animation ait le temps de s'apprécier
         setTimeout(() => {
             loader.classList.add('hidden');
-        }, 400);
+        }, 1300);
     }
 });
